2f9f35e1e5c461e6da62fc7977f4bfaf2e5f1f68
Brennan Manning <manningb24@gmail.com>
2022-01-21 19:52:09 -0500

    First commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function App() {
+  const [text, onChangeText] = React.useState<string | undefined>();
+  const [number, onChangeNumber] = React.useState<string | undefined>();
+  const [timeChosen, onChoseTime] = React.useState<boolean>(false);
+  const [time, onSelectedTime] = React.useState<string>();
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.message}>I want to reminded to</Text>
+      <TextInput
+        style={styles.input}
+        value={text}
+        onChangeText={onChangeText}
+        placeholder="Drinking Water"
+      />
+      <Text style={styles.message}> every</Text>
+      <TextInput
+        style={styles.input}
+        value={number}
+        onChangeText={onChangeNumber}
+        placeholder="15"
+        keyboardType="numeric"
+      />
+      {timeChosen && <Text style={styles.message}>{time}</Text>}
+      {!timeChosen && (
+        <View style={styles.listStyle}>
+          <TouchableOpacity
+            onPress={() => {
+              onChoseTime(true);
+              onSelectedTime("minutes");
+            }}
+            style={styles.timeButton}
+          >
+            <Text style={styles.time}>minutes</Text>
+          </TouchableOpacity>
+          <View style={styles.listSpacer} />
+          <TouchableOpacity
+            onPress={() => {
+              onChoseTime(true);
+              onSelectedTime("hours");
+            }}
+            style={styles.timeButton}
+          >
+            <Text style={styles.time}>hours</Text>
+          </TouchableOpacity>
+          <View style={styles.listSpacer} />
+          <TouchableOpacity
+            onPress={() => {
+              onChoseTime(true);
+              onSelectedTime("days");
+            }}
+            style={styles.timeButton}
+          >
+            <Text style={styles.time}>days</Text>
+          </TouchableOpacity>
+        </View>
+      )}
+      <TouchableOpacity
+        onPress={() =>
+          alert(
+            `Notifying you to ${text?.toLowerCase()} every ${number} ${time}`
+          )
+        }
+        style={styles.confirmButton}
+      >
+        <Text style={styles.time}> Confirm </Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +82,51 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  head: {
+    flexDirection: "row",
+  },
+
+  message: {
+    fontSize: 24,
+    color: "#888",
+  },
+
+  input: {
+    borderStyle: "solid",
+    borderBottomWidth: 3,
+    borderBottomColor: "#dbdbdb",
+    color: "#888",
+    fontSize: 24,
+  },
+
+  time: {
+    fontSize: 24,
+    color: "#fff",
+  },
+
+  timeButton: {
+    backgroundColor: "#cccccc",
+    paddingHorizontal: 4,
+  },
+
+  listStyle: {
+    flexDirection: "row",
+    marginTop: 4,
+  },
+
+  listSpacer: {
+    paddingHorizontal: 2,
+  },
+
+  confirmButton: {
+    marginTop: 10,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    backgroundColor: "#b2b2b2",
   },
 });
