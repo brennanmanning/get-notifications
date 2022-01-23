ffaa3ce493f274911d173bd480692791cc4ba577
Brennan Manning <manningb24@gmail.com>
2022-01-23 15:20:02 -0500

    Configured Basic Navigation

--- App.tsx
+++ App.tsx
@@ -7,7 +7,8 @@ import {
   View,
 } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
 
 function HomeScreen() {
   enum TIMES {
@@ -136,14 +137,42 @@ function HomeScreen() {
   );
 }
 
-const Stack = createNativeStackNavigator();
+function DetailsScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.message}> Details Screen </Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === "Home") {
+              iconName = focused
+                ? "ios-information-circle"
+                : "ios-information-circle-outline";
+            } else if (route.name === "Details") {
+              iconName = "ios-list";
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: "tomato",
+          tabBarInactiveTintColor: "gray",
+        })}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Details" component={DetailsScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
